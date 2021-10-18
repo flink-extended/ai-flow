4c51c9b4c8a4be6a1abc5814fda6f8e82b537e40
Xuannan <suxuannan95@gmail.com>
2021-10-18 10:59:30 +0800

    [AIFlow] Fix oss blob manager download concurrently (#3)
    * [Airflow] StandardTaskRunner log exception when fail
    
    * [AIFlow] Fix oss blob manager download concurrently
    
    * [AIFlow] Fix test
    
    (cherry picked from commit a2b5fc8341e99387c7bd8bcba76caf9c7120884d)

--- ai_flow_plugins/tests/blob_manager_plugins/test_oss_blob_manager.py
+++ ai_flow_plugins/tests/blob_manager_plugins/test_oss_blob_manager.py
@@ -16,10 +16,15 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+import threading
 import unittest
+from unittest import mock
+
 import os
+
 from ai_flow.util.path_util import get_file_dir
 from ai_flow.plugin_interface.blob_manager_interface import BlobConfig, BlobManagerFactory
+from ai_flow_plugins.blob_manager_plugins.oss_blob_manager import OssBlobManager
 
 
 class TestOSSBlobManager(unittest.TestCase):
@@ -51,6 +56,73 @@ class TestOSSBlobManager(unittest.TestCase):
         downloaded_path = blob_manager.download_project('1', uploaded_path)
         self.assertEqual('/tmp/workflow_1_project/project', downloaded_path)
 
+    def test_download_oss_file_concurrently(self):
+        config = {}
+        oss_blob_manager = OssBlobManager(config)
+
+        zip_file_path = None
+        call_count = 0
+
+        def mock_get_oss_object(dest, oss_object_key):
+            nonlocal zip_file_path, call_count
+            call_count += 1
+            zip_file_path = dest
+            with open(dest, 'w') as f:
+                pass
+
+        oss_blob_manager._get_oss_object = mock_get_oss_object
+
+        # get_oss_object_func = mock.patch.object(oss_blob_manager, '_get_oss_object', wraps=mock_get_oss_object)
+        with mock.patch(
+                'ai_flow_plugins.blob_manager_plugins.oss_blob_manager.extract_project_zip_file'):
+
+            def download_loop():
+                for i in range(1000):
+                    oss_blob_manager.download_project('1', 'dummy_path', '/tmp')
+
+            try:
+                t1 = threading.Thread(target=download_loop)
+                t1.start()
+
+                download_loop()
+                t1.join()
+
+                self.assertEqual(1, call_count)
+            finally:
+                if zip_file_path is not None:
+                    os.remove(zip_file_path)
+
+    def test_lazily_init_bucket(self):
+        config = {}
+        oss_blob_manager = OssBlobManager(config)
+        self.assertIsNone(oss_blob_manager._bucket)
+
+        with mock.patch('ai_flow_plugins.blob_manager_plugins.oss_blob_manager.oss2') as mock_oss:
+            mock_bucket = mock.Mock()
+            mock_oss.Bucket.return_value = mock_bucket
+            bucket = oss_blob_manager.bucket
+            mock_oss.Auth.assert_called_once()
+            mock_oss.Bucket.assert_called_once()
+            self.assertEqual(mock_bucket, bucket)
+            self.assertEqual(mock_bucket, oss_blob_manager.bucket)
+
+    def test__get_oss_object_retry(self):
+        config = {}
+        oss_blob_manager = OssBlobManager(config)
+
+        with mock.patch.object(oss_blob_manager, '_bucket') as mock_bucket:
+            mock_bucket.get_object_to_file.side_effect = [RuntimeError("boom"), RuntimeError("boom"),
+                                                          RuntimeError("boom")]
+            with self.assertRaises(RuntimeError):
+                oss_blob_manager._get_oss_object('dummy_dest', 'key', retry_sleep_sec=0.1)
+                self.assertEqual(3, mock_bucket.get_object_to_file.call_count)
+
+        with mock.patch.object(oss_blob_manager, '_bucket') as mock_bucket:
+            mock_bucket.get_object_to_file.side_effect = [RuntimeError("boom"), None]
+            oss_blob_manager._get_oss_object('dummy_dest', 'key', retry_sleep_sec=0.1)
+            self.assertEqual(2, mock_bucket.get_object_to_file.call_count)
+
 
 if __name__ == '__main__':
     unittest.main()
+
