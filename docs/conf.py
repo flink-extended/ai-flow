a1a4c3e5462cc7ae6daad3abacf41a5e9c8b156b
bgeng777 <gengbiao.gb@alibaba-inc.com>
2021-10-22 15:45:10 +0800

    Fix author name to flink-extended

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../ai_flow_plugins'))
 # -- Project information -----------------------------------------------------
 
 project = 'AI Flow'
-author = 'flink-ai-extended'
+author = 'flink-extended'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
