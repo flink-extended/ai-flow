d54ecb0be6b0420a457a9e5f7c8e379271f5c9e2
sxnan <suxuannan95@gmail.com>
2021-10-26 17:32:33 +0800

    [Airflow] Allow LocalExecutor run with sqlite

--- lib/airflow/airflow/configuration.py
+++ lib/airflow/airflow/configuration.py
@@ -228,6 +228,7 @@ class AirflowConfigParser(ConfigParser):  # pylint: disable=too-many-ancestors
         is_executor_without_sqlite_support = self.get("core", "executor") not in (
             'DebugExecutor',
             'SequentialExecutor',
+            'LocalExecutor',
         )
         is_sqlite = "sqlite" in self.get('core', 'sql_alchemy_conn')
         if is_executor_without_sqlite_support and is_sqlite:
