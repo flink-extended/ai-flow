5e7f8fe8335e3977d9d8f89846fd385359dcc002
jiangxin <jiangxin.jiang@alibaba-inc.com>
2021-12-23 09:59:46 +0800

    fix lint error

--- ai_flow/test/api/ut_workflows/workflows/test_workflow_operation/test_workflow_operation.py
+++ ai_flow/test/api/ut_workflows/workflows/test_workflow_operation/test_workflow_operation.py
@@ -14,6 +14,7 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
+import shutil
 import unittest
 import os
 from ai_flow.ai_graph.ai_graph import current_graph
