290fd127d9b4fddd0db7faaa807f467b1868a47b
jiangxin <jiangxin.jiang@alibaba-inc.com>
2022-02-21 16:29:15 +0800

    Remove unnecessary mongo dependency

--- lib/notification_service/tests/test_service.py
+++ lib/notification_service/tests/test_service.py
@@ -1,13 +1,13 @@
 import unittest
 
 from notification_service.event_storage import DbEventStorage
-from notification_service.mongo_event_storage import MongoEventStorage
 from notification_service.service import NotificationService
 
 
 class NotificationServiceTest(unittest.TestCase):
 
     def test_from_storage_uri(self):
+        from notification_service.mongo_event_storage import MongoEventStorage
         service = NotificationService.from_storage_uri("sqlite:///aiflow.db")
         self.assertTrue(isinstance(service.storage, DbEventStorage))
         service = NotificationService.from_storage_uri("mongodb://user:pass@localhost:27017/test")
