1c29412179d042f5589a58985090d7e373257462
jiangxin <jiangxin.jiang@alibaba-inc.com>
2022-08-24 15:14:13 +0800

    1

--- samples/action_on_condition/condition_example.py
+++ samples/action_on_condition/condition_example.py
@@ -14,10 +14,31 @@
 # specific language governing permissions and limitations
 # under the License.
 #
+from ai_flow.model.action import TaskAction
 from ai_flow.model.workflow import Workflow
+from ai_flow.operators.bash import BashOperator
+
+
+class StateCondition(Condition):
+
+    def is_met(self, event: Event, context: Context) -> bool:
+        state: ValueState = context.get_state(ValueStateDescriptor(name='count'))
+        v = state.value()
+        if v is None:
+            v = 0
+        v = v + 1
+        state.update(v)
+        if 0 == v % 2:
+            return True
+        else:
+            return False
+
 
 with Workflow(name='condition_workflow') as workflow:
-    task1 =
+    task1 = BashOperator(name='task1',
+                         bash_command='echo 1')
+
+    task1.action_on_condition(TaskAction.START, )
 
     batch_jar = expand_env_var('${FLINK_HOME}/examples/batch/WordCount.jar')
     streaming_jar = expand_env_var('${FLINK_HOME}/examples/streaming/TopSpeedWindowing.jar')
