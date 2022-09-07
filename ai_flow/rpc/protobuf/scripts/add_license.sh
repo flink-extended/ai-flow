15092684f7e0f94305cbc5d352b98005af6e93ad
JiangXin <jiangxin.jiang@alibaba-inc.com>
2022-09-07 14:19:33 +0800

    Replace the incorrect license (#379)

--- ai_flow/rpc/protobuf/scripts/add_license.sh
+++ ai_flow/rpc/protobuf/scripts/add_license.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 ##
-## Licensed to the Apache Software Foundation (ASF) under one
-## or more contributor license agreements.  See the NOTICE file
-## distributed with this work for additional information
-## regarding copyright ownership.  The ASF licenses this file
-## to you under the Apache License, Version 2.0 (the
-## "License"); you may not use this file except in compliance
-## with the License.  You may obtain a copy of the License at
+## Copyright 2022 The AI Flow Authors
+##
+## Licensed under the Apache License, Version 2.0 (the "License");
+## you may not use this file except in compliance with the License.
+## You may obtain a copy of the License at
 ##
 ##   http://www.apache.org/licenses/LICENSE-2.0
 ##
@@ -29,13 +27,11 @@ do
    if ! [[ $i == *"__init__"* ]]; then
      sed -i '' -e '1i \
 #\
-# Licensed to the Apache Software Foundation (ASF) under one\
-# or more contributor license agreements.  See the NOTICE file\
-# distributed with this work for additional information\
-# regarding copyright ownership.  The ASF licenses this file\
-# to you under the Apache License, Version 2.0 (the\
-# "License"); you may not use this file except in compliance\
-# with the License.  You may obtain a copy of the License at\
+# Copyright 2022 The AI Flow Authors\
+#\
+# Licensed under the Apache License, Version 2.0 (the "License");\
+# you may not use this file except in compliance with the License.\
+# You may obtain a copy of the License at\
 #\
 #   http://www.apache.org/licenses/LICENSE-2.0\
 #\
@@ -46,34 +42,11 @@ do
 # specific language governing permissions and limitations\
 # under the License.\
 #\
+\
 ' $i
    fi
 
 done
-
-# Add licenses to generated java files
-#for i in ${java_dir}/*.java
-#do
-#   sed -i '' -e '1i \
-#/*\
-#\ * Licensed to the Apache Software Foundation (ASF) under one\
-#\ * or more contributor license agreements.  See the NOTICE file\
-#\ * distributed with this work for additional information\
-#\ * regarding copyright ownership.  The ASF licenses this file\
-#\ * to you under the Apache License, Version 2.0 (the\
-#\ * "License"); you may not use this file except in compliance\
-#\ * with the License.  You may obtain a copy of the License at\
-#\ *\
-#\ *   http://www.apache.org/licenses/LICENSE-2.0\
-#\ *\
-#\ * Unless required by applicable law or agreed to in writing,\
-#\ * software distributed under the License is distributed on an\
-#\ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY\
-#\ * KIND, either express or implied.  See the License for the\
-#\ * specific language governing permissions and limitations\
-#\ * under the License.\
-#\ */\' $i
-#done
 
 
 # Add licenses to generated go files
@@ -81,13 +54,11 @@ for i in ${go_dir}/*.gw.go
 do
    sed -i '' -e '1i \
 //\
-// Licensed to the Apache Software Foundation (ASF) under one\
-// or more contributor license agreements.  See the NOTICE file\
-// distributed with this work for additional information\
-// regarding copyright ownership.  The ASF licenses this file\
-// to you under the Apache License, Version 2.0 (the\
-// "License"); you may not use this file except in compliance\
-// with the License.  You may obtain a copy of the License at\
+// Copyright 2022 The AI Flow Authors\
+//\
+// Licensed under the Apache License, Version 2.0 (the "License");\
+// you may not use this file except in compliance with the License.\
+// You may obtain a copy of the License at\
 //\
 //     http://www.apache.org/licenses/LICENSE-2.0\
 //\
@@ -96,7 +67,8 @@ do
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\
 // See the License for the specific language governing permissions and\
 // limitations under the License.\
-\' $i
+\
+' $i
 done
 
 
